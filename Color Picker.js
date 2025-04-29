19e086a84c3436a392870dbe3744b8d7b4749eb3
Mamedibathula Tulasi <112938030+tulasi1013@users.noreply.github.com>
2025-04-29 22:47:53 +0530

    Add files via upload

---
+++ Color Picker.js
@@ -0,0 +1,22 @@
+let containerElement = document.getElementById("colorPickerContainer");
+let colorElement = document.getElementById("selectedColorHexCode");
+
+function button1() {
+    containerElement.style.backgroundColor = "#e0e0e0";
+    colorElement.textContent = "#e0e0e0";
+}
+
+function button2() {
+    containerElement.style.backgroundColor = "#6fcf97";
+    colorElement.textContent = "#6fcf97";
+}
+
+function button3() {
+    containerElement.style.backgroundColor = "#56ccf2";
+    colorElement.textContent = "#56ccf2";
+}
+
+function button4() {
+    containerElement.style.backgroundColor = "#bb6bd9";
+    colorElement.textContent = "#bb6bd9";
+}
